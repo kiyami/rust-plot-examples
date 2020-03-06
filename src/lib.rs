9aa875964e64e10ec8defc4555487854219b6ecf
kiyami <kiyami_erdim@hotmail.com>
2020-03-06 16:41:18 +0300

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use plotters::prelude::*;
+
+use rand::SeedableRng;
+use rand_distr::{Distribution, Normal};
+use rand_xorshift::XorShiftRng;
+
+use itertools::Itertools;
+
+use num_traits::sign::Signed;
+
+pub fn scatter(data: Vec<(f64,f64)>) -> Result<(), Box<dyn std::error::Error>> {
+
+    let root = BitMapBackend::new("kym.png", (1024, 768)).into_drawing_area();
+    root.fill(&WHITE);
+
+    let mut chart = ChartBuilder::on(&root)
+        .caption("my_plot", ("sans-serif", 60))
+        .margin(10)
+        .set_label_area_size(LabelAreaPosition::Left, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .build_ranged(0f64..10f64, 0f64..10f64)?;
+
+    chart.configure_mesh().draw()?;
+
+    chart.draw_series(LineSeries::new(data, &GREEN.mix(10.0)))?
+         .label("Raw Data")
+         .legend(|(x,y)| PathElement::new(vec![(x,y), (x+20,y)], &GREEN));
+
+    chart.configure_series_labels()
+         .background_style(WHITE.filled())
+         .draw()?;
+
+    Ok(())
+}
